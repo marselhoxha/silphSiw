1a0f42bc0a09a34316cf6930651f3eaa321dd212
Marsel Hoxha <marsel.hox@gmail.com>
2019-06-19 18:55:53 +0200

    Added exception classes

---
+++ src/main/java/it/uniroma3/silphSiw/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package it.uniroma3.silphSiw.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
