ce733d28b4e2054a2d3d4aaae5ad3ec9e87b35b6
Marsel Hoxha <marsel.hox@gmail.com>
2019-06-19 15:10:06 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/silphSiw/SilphSiwApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.silphSiw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilphSiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilphSiwApplication.class, args);
+	}
+
+}
